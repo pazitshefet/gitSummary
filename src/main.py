d54a24dc82e4d61d8ce9f7a56948a90bf0785e0c
pshefet <pazit.shefet@starhomemach.com>
2026-02-24 15:13:22 +0200

    first ver

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import argparse
+import uvicorn
+
+def main():
+    parser = argparse.ArgumentParser(description="Summarize a GitHub repository with an LLM")
+    parser.add_argument("--host",
+                        default="127.0.0.1",
+                        help="FastAPI host ip (default: 127.0.0.1)")
+    parser.add_argument("--port",
+                        type=int,
+                        default=8080,
+                        help="FastAPI port (default: 8080)")
+    args = parser.parse_args()
+
+    uvicorn.run("web_api:app", host=args.host, port=args.port)
+
+if __name__ == "__main__":
+    main()
