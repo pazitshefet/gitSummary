7005ee72dd24d58d6d4cb4f75c05167dfa3b87e3
pshefet <pazit.shefet@starhomemach.com>
2026-02-24 18:36:50 +0200

    updates

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ def main():
                         help="FastAPI host ip (default: 127.0.0.1)")
     parser.add_argument("--port",
                         type=int,
-                        default=8080,
-                        help="FastAPI port (default: 8080)")
+                        default=8000,
+                        help="FastAPI port (default: 8000)")
     args = parser.parse_args()
 
     uvicorn.run("web_api:app", host=args.host, port=args.port)
